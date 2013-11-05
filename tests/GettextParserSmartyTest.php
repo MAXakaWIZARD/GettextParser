3c771e132ce7c193efc9b903a7b25299e0d07809
MAXakaWIZARD <maxakawizard@gmail.com>
2013-11-05 18:18:04 +0200

    add test data

--- tests/GettextParserSmartyTest.php
+++ tests/GettextParserSmartyTest.php
@@ -67,6 +67,14 @@ class GettextParserSmartyTest extends PHPUnit_Framework_TestCase
             array(
                 '{"Text to be localized"|_}',
                 array('Text to be localized')
+            ),
+            array(
+                '{t}User %FROM_EMAIL% has shared a video "%PLAYLIST_TITLE%" with you.{/t}',
+                array('User %FROM_EMAIL% has shared a video "%PLAYLIST_TITLE%" with you.')
+            ),
+            array(
+                '{t}You can watch it <a href="%PLAYLIST_URL%">here</a>{/t}',
+                array('You can watch it <a href="%PLAYLIST_URL%">here</a>')
             )
         );
     }
